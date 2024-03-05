d91bd72363e7b52760ffaed77ef2d66ea9cfa2a2
erfan <erfan33200@gmail.com>
2024-03-05 18:38:41 +0330

    random integers

--- HW2/Hw2.py
+++ HW2/Hw2.py
@@ -6,6 +6,7 @@ def find_duplicates(arr):
     duplicate=[num for num,count in counts.items() if count > 1]
     return duplicate
 
+
 random.seed(400121055)
 rand_numbers=[random.randint(1,21) for _ in range(1000)]
 result=find_duplicates(rand_numbers)
