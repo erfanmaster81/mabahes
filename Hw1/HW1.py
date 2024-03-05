0e23b7c88ebe6d76a6041f9de367a5f9312cac7b
erfan <erfan33200@gmail.com>
2024-03-05 17:29:42 +0330

    visible

---
+++ Hw1/HW1.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(0,2*np.pi,.01)
+y1 = np.sin (x)
+y2 = np.cos (x)
+
+plt.grid()
+plt.plot(x,y1)
+plt.plot(x,y2)
+plt.fill_between(x,y1,y2,hatch='//',color='gray',fc='white')
+plt.show()
+
---
+++ Hw2/Hw2.py
@@ -0,0 +1,13 @@
+import random
+from collections import Counter
+
+def find_duplicates(arr):
+    counts=Counter(arr)
+    duplicate=[num for num,count in counts.items() if count > 1]
+    return duplicate
+
+random.seed(400121055)
+rand_numbers=[random.randint(1,1000) for _ in range(1000)]
+result=find_duplicates(rand_numbers)
+print(result)
+
